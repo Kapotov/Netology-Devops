ec0d6ab4a9124841c1989856d3295c2bade04adb
Kapotov <vitakapo@mail.ru>
2023-02-06 22:12:12 +0300

    git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,17 +1,26 @@
-
-
-
-
-
-
-
 #!/bin/bash
 # display command line options
 
 count=1
 for param in "$@"; do
-    echo "Parameter: $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
 echo "====="
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
